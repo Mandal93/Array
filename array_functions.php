271d4c7dab29388c4734dff6e945285acfec81af
Bpull Mandal <notd2256@gmail.com>
2025-12-14 23:08:42 +0545

    Add files via upload

---
+++ array_functions.php
@@ -0,0 +1,13 @@
+<?php
+
+$arr=[1,2,3,4,5,6,7,8];
+// $new_array=array_slice($arr,2,4);
+ 
+// var_dump($arr);
+// //var_dump($new_array);
+// array_splice($arr,1,3,["ram","shyam","hari"]);
+//removing items and replacing with new items
+$n_a=array_slice($arr,-1);
+print_r($n_a);
+
+?>  
